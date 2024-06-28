8c1555ccfc8bbc83b0113da894f903754d5cb9ee
daniel.carvalho <carvalho.rochadan@gmail.com>
2024-06-27 23:58:18 -0300

    feat: add Loader component, add lazy loading

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home, Wishlist, NotFound } from "./pages";
+import { Wishlist } from "./pages";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader/Loader";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
 
 function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="wishlist" element={<Wishlist />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
